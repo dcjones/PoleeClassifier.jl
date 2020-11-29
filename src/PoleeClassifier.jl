91ce56fb803c70fe8f10c374db92dc49a1353ae2
Daniel C. Jones <dcjones@cs.washington.edu>
2020-11-29 11:26:15 -0800

    Attempt at classifying using beta ptt params directly

--- src/PoleeClassifier.jl
+++ src/PoleeClassifier.jl
@@ -5,9 +5,13 @@ using Polee
 using Polee.PoleeModel
 using Flux
 using HDF5
+using ProgressMeter
 
 const device = Flux.gpu
 # const device = Flux.cpu
+
+const batchsize = 50
+const nepochs = 1000
 
 # Different ways of approaching quantification each with a different take
 # on training the clasifier
@@ -45,6 +49,9 @@ function build_model(n_in::Int, n_out::Int)
     M = 50
     return Chain(
         Dense(n_in, M, leakyrelu, initW=initW),
+        Dropout(0.25, M),
+        Dense(M, M, leakyrelu, initW=initW),
+        Dropout(0.25, M),
         Dense(M, M, leakyrelu, initW=initW),
         Dense(M, n_out, initW=initW))
 end
@@ -69,7 +76,7 @@ function fit!(model::Classifier{PointEstimate}, train_spec::Dict)
 jj
     train_data_loader = device.(Flux.Data.DataLoader(
         train_expr, train_classes,
-        batchsize=50, shuffle=true))
+        batchsize=batchsize, shuffle=true))
 
     function total_loss()
         l = 0f0
@@ -79,8 +86,8 @@ jj
         return l / length(train_data_loader)
     end
 
-    opt = ADAM(1e-3)
-    Flux.@epochs 300 Flux.Optimise.train!(
+    opt = ADAM()
+    Flux.@epochs nepochs Flux.Optimise.train!(
             (x, y) -> Flux.Losses.logitcrossentropy(model.layers(x), y),
             params(model.layers),
             train_data_loader,
@@ -90,6 +97,8 @@ end
 
 
 function eval(model::Classifier{PointEstimate}, eval_spec::Dict)
+    Flux.testmode!(model.layers)
+
     eval_data = load_point_estimates_from_specification(
         eval_spec,
         model.quant.ts,
@@ -113,7 +122,7 @@ struct KallistoBootstrap <: QuantMethod
     ts::Polee.Transcripts
     ts_metadata::Polee.TranscriptsMetadata
     pseudocount::Float32
-    # TODO: parameter giving the maximum number of bootstrap samples to use.
+    max_boot_samples::Union{Nothing, Int}
 end
 
 
@@ -130,7 +139,6 @@ function load_kallisto_bootstap(model::Classifier{KallistoBootstrap}, spec::Dict
 
     for sample in spec["samples"]
         filename = sample["kallisto"]
-        @show filename
         input = h5open(filename)
 
         transcript_ids = read(input["aux"]["ids"])
@@ -139,13 +147,19 @@ function load_kallisto_bootstap(model::Classifier{KallistoBootstrap}, spec::Dict
         label = sample["factors"][model.factor]
         label_onehot = Flux.onehot(label, model.classes)
 
+        boot_count = 0
         for dataset in input["bootstrap"]
+            if model.quant.max_boot_samples !== nothing && boot_count >= model.quant.max_boot_samples
+                break
+            end
+
             bootstrap_counts = Vector{Float32}(read(dataset))
             bs = Polee.PoleeModel.kallisto_counts_to_proportions(
                 bootstrap_counts, efflens, model.quant.pseudocount,
                 transcript_ids, transcript_idx)
             push!(expr_data_vecs, bs)
             push!(class_data_vecs, label_onehot)
+            boot_count += 1
         end
 
         close(input)
@@ -166,7 +180,7 @@ function fit!(model::Classifier{KallistoBootstrap}, train_spec::Dict)
 
     train_data_loader = device.(Flux.Data.DataLoader(
         train_expr, train_classes,
-        batchsize=50, shuffle=true))
+        batchsize=batchsize, shuffle=true))
 
     function total_loss()
         l = 0f0
@@ -176,8 +190,8 @@ function fit!(model::Classifier{KallistoBootstrap}, train_spec::Dict)
         return l / length(train_data_loader)
     end
 
-    opt = ADAM(1e-3)
-    Flux.@epochs 300 Flux.Optimise.train!(
+    opt = ADAM()
+    Flux.@epochs nepochs Flux.Optimise.train!(
             (x, y) -> Flux.Losses.logitcrossentropy(model.layers(x), y),
             params(model.layers),
             train_data_loader,
@@ -187,6 +201,8 @@ end
 
 
 function eval(model::Classifier{KallistoBootstrap}, eval_spec::Dict)
+    Flux.testmode!(model.layers)
+
     transcript_idx = Dict{String, Int}()
     for (j, t) in enumerate(model.quant.ts)
         transcript_idx[t.metadata.name] = j
@@ -196,7 +212,6 @@ function eval(model::Classifier{KallistoBootstrap}, eval_spec::Dict)
     acc = 0.0
     for sample in eval_spec["samples"]
         filename = sample["kallisto"]
-        @show filename
         input = h5open(filename)
 
         transcript_ids = read(input["aux"]["ids"])
@@ -206,18 +221,23 @@ function eval(model::Classifier{KallistoBootstrap}, eval_spec::Dict)
         label_onehot = Flux.onehot(label, model.classes)
 
         pred = zeros(Float32, length(model.classes))
-        pred_count = 0
+        boot_count = 0
 
+        # TODO: Actually we should be averaging after softmax, not prior
         for dataset in input["bootstrap"]
+            if model.quant.max_boot_samples !== nothing && boot_count >= model.quant.max_boot_samples
+                break
+            end
+
             bootstrap_counts = Vector{Float32}(read(dataset))
             bs = Polee.PoleeModel.kallisto_counts_to_proportions(
                 bootstrap_counts, efflens, model.quant.pseudocount,
                 transcript_ids, transcript_idx)
 
             pred .+= cpu(model.layers(device(log.(bs[1,:]) .- log(1/n))))
-            pred_count += 1
+            boot_count += 1
         end
-        pred ./= pred_count
+        pred ./= boot_count
         acc += Flux.onecold(pred) .== Flux.onecold(label_onehot)
         close(input)
     end
@@ -227,12 +247,79 @@ function eval(model::Classifier{KallistoBootstrap}, eval_spec::Dict)
 end
 
 
+struct PTTBetaParams <: QuantMethod
+end
+
+
+function load_beta_params(model::Classifier{PTTBetaParams}, spec::Dict)
+    xs_vecs = Vector{Float32}[]
+    for sample in spec["samples"]
+        input = h5open(sample["file"])
+        x = vcat(read(input["alpha"]), read(input["beta"]))
+        push!(xs_vecs, x)
+        close(input)
+    end
+
+    train_approx = log.(hcat(xs_vecs...))
+    train_classes = hcat(
+        [Flux.onehot(sample["factors"][model.factor], model.classes)
+         for sample in spec["samples"]]...)
+
+    return train_approx, train_classes
+end
+
+
+function fit!(model::Classifier{PTTBetaParams}, train_spec::Dict)
+
+    model.classes = get_classes(train_spec, model.factor)
+    train_approx, train_classes = load_beta_params(model, train_spec)
+
+    train_data_loader = device.(Flux.Data.DataLoader(
+        train_approx, train_classes,
+        batchsize=batchsize, shuffle=true))
+
+    n_in = size(train_approx, 1)
+    n_out = length(model.classes)
+    model.layers = device(build_model(n_in, n_out))
+
+    function total_loss()
+        l = 0f0
+        for (x, y) in train_data_loader
+            l += Flux.Losses.logitcrossentropy(model.layers(x), y)
+        end
+        return l / length(train_data_loader)
+    end
+
+    opt = ADAM()
+    Flux.@epochs nepochs Flux.Optimise.train!(
+            (x, y) -> Flux.Losses.logitcrossentropy(model.layers(x), y),
+            params(model.layers),
+            train_data_loader,
+            opt,
+            cb=() -> @show total_loss())
+end
+
+
+function eval(model::Classifier{PTTBetaParams}, eval_spec::Dict)
+    Flux.testmode!(model.layers)
+
+    eval_approx, eval_classes = load_beta_params(model, eval_spec)
+
+    n_in, num_samples = size(eval_approx)
+
+    acc = 0.0
+    for i in 1:num_samples
+        pred = cpu(model.layers(device(eval_approx[:,i])))
+        acc += eval_spec["samples"][i]["factors"][model.factor] == model.classes[Flux.onecold(pred)]
+    end
+    acc /= num_samples
+    return acc
+end
+
+
 # TODO: Figure out how to set up a sampler as the input to the model. Probably
 # a custom training loop is the easiest way to pull that off.
 struct PTTLatentExpr <: QuantMethod end
-
-# TODO: Do beta-ptt approximation then use vcat(α, β) as the input.
-struct PTTBetaParams <: QuantMethod end
 
 
 end # module PoleeClassifier
